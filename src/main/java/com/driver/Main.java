4f49edb3eecfd93087873924bf3f075554274c9e
Mayuri <bhamaremayuri12@gmail.com>
2023-08-13 22:51:00 +0530

    Assignment 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args)
+  {
+      RWOnly obj = new RWOnly();
+      obj.name = "mayuri"; //cannot access private member
+      obj.setName("mayuri");
+      obj.getName();
+  }
 }
